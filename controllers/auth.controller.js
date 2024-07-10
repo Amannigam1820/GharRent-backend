7749ac9c88466d6ebba1bace867cdbd7bb1dceaa
Aman <amannigam18082001@gmail.com>
2024-07-10 16:46:50 +0530

    add authentication and authorization functionality

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,13 +1,19 @@
 import bcrypt from "bcrypt";
 import prisma from "../lib/prisma.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   const { username, email, contact, password } = req.body;
 
+  //console.log(username,email,contact,password);
+
   // HASH THE PASSWORD FIRST
 
   try {
+    const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    //console.log(hashedPassword);
 
     const newUser = await prisma.user.create({
       data: {
@@ -23,12 +29,70 @@ export const register = async (req, res) => {
       user: newUser,
     });
   } catch (error) {
+    console.log(error);
     res.status(500).json({
       message: "Failed to create user",
     });
   }
 };
 
-export const login = (req, res) => {};
+export const login = async (req, res) => {
+  const { username, password } = req.body;
 
-export const logout = (req, res) => {};
+  try {
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid Credentials" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid Credentials" });
+    }
+
+    const age = 1000 * 60 * 60 * 24 * 7;
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        isAdmin :false
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: age }
+    );
+
+    const { password: userPassword, ...userInfo } = user;
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        maxAge: age,
+      })
+      .status(200)
+      .json({ message: "Login SuccessFully", userInfo:userInfo });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to login!" });
+  }
+};
+
+export const logout = (req, res) => {
+  res.clearCookie("token").status(200).json({ message: "Logout SuccessFully" });
+};
+
+
+
+export const getMyProfile = async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: req.userId,
+    },
+  });
+  res.status(200).json({
+    success: true,
+    user,
+  })
+}
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,14 +1,15 @@
-import express from "express"
-import {login, logout, register} from "../controllers/auth.controller.js"
-
+import express from "express";
+import { getMyProfile, login, logout, register } from "../controllers/auth.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-router.post("/register",register);
+router.post("/register", register);
 
 router.post("/login", login);
 
-router.post("/logout", logout );
+router.post("/logout", logout);
 
+router.get("/me",verifyToken, getMyProfile)
 
 export default router;
